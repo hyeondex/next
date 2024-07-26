b1c081f6957c39251ceb3445443a4ce47be30781
[이모션] 이수현 <soohyeon.lee@emotion.co.kr>
2024-07-26 16:02:14 +0900

    pagination add

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -6,14 +6,6 @@ import {CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton} from "@/app/
 import {Suspense} from "react";
 
 export default async function Page() {
-
-    //const revenue = await fetchRevenue();
-    //const latestInvoices = await fetchLatestInvoices();
-
-    // const {  numberOfCustomers,
-    //     numberOfInvoices,
-    //     totalPaidInvoices,
-    //     totalPendingInvoices,} =await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,15 +15,6 @@ export default async function Page() {
                 <Suspense fallback={<CardSkeleton/>}>
                     <CardWrapper/>
                 </Suspense>
-
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />*/}
-                {/* <Card title="Pending" value={totalPendingInvoices} type="pending" />*/}
-                {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />*/}
-                {/*{<Card*/}
-                {/*  title="Total Customers"*/}
-                {/*  value={numberOfCustomers}*/}
-                {/*  type="customers"*/}
-                {/*/>}*/}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton/>}>
